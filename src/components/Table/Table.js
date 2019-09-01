d8de8c8bc92df112d15b4e39d342ef8a57301d33
lao <ogunlao@yahoo.com>
2019-09-01 13:11:26 +0100

    Refactor table component to use table tag

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,32 +1,31 @@
 import React from 'react';
 import Button from '../Button'
+import './Table.css';
 
 const Table = ({ list, onDismiss }) =>
-  <div className="table">
+  <div >
+    <table className="table">
+      <tr className="table-header">
+        <th>Article Title</th>
+      </tr>
     {list.map(item =>
-      <div key={item.objectID} className="table-row">
-        <span style={{ width: '40%' }}>
-          <a href={item.url}>{item.title}</a>
-        </span>
-        <span style={{ width: '30%' }}>
-          {item.author}
-        </span>
-        <span style={{ width: '10%' }}>
-          {item.num_comments}
-        </span>
-        <span style={{ width: '10%' }}>
-          {item.points}
-        </span>
-        <span style={{ width: '10%' }}>
-          <Button
-            onClick={() => onDismiss(item.objectID)}
-            className="button-inline"
-          >
-            Dismiss
+        <tr key={item.objectID} className="table-row">
+          <td style={{ width: '40%' }}><a href={item.url}>{item.title}</a></td>
+          <td style={{ width: '30%' }}>{item.author}</td>
+          <td style={{ width: '10%' }}>{item.num_comments}</td>
+          <td style={{ width: '10%' }}>{item.points}</td>
+          <td>
+              <Button
+                onClick={() => onDismiss(item.objectID)}
+                className="button-inline"
+              >
+                Dismiss
               </Button>
-        </span>
-      </div>
+            </td>
+        </tr>
+    
     )}
+    </table>
   </div>
 
 export default Table;
